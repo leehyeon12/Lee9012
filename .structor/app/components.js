2589369c78966c15a7460fd9fd18c3c3359d959f
ruucm <ruucm.a@gmail.com>
2018-07-12 21:44:06 +0700

    exta

---
+++ .structor/app/components.js
@@ -0,0 +1,16 @@
+import '../../app/appStyle'
+
+import * as Centering from 'modules/Centering';
+import * as Animations from 'modules/Animations';
+import * as Router from 'modules/Router';
+import * as Layout from 'modules/Layout';
+import * as Test2 from 'modules/Test2';
+import * as MycComponents from 'modules/MycComponents';
+export {
+    Centering,
+    Animations,
+    Router,
+    Layout,
+    Test2,
+    MycComponents
+};
